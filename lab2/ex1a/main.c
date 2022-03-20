95423c107f996ac17181e1fe6b19ec5e04d340aa
Cozoob <90906410+Cozoob@users.noreply.github.com>
2022-03-20 11:57:57 +0100

    Add ex1a done
    Really easy...

---
+++ lab2/ex1a/main.c
@@ -0,0 +1,84 @@
+// Created by Marcin Kozub using fread() and fwrite()
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void scanf_error();
+
+int main(int argc, char* argv[]) {
+
+    if(argc == 2){
+        printf("%d\n", argc);
+        perror("There are not enough arguments!");
+        exit(-1);
+    }
+
+    if(argc > 3){
+        perror("There are too many arguments!");
+        exit(-1);
+    }
+
+    char* filename1 = calloc(256, sizeof (char));
+    char* filename2 = calloc(256, sizeof (char));
+
+    unsigned int i;
+
+    if(argc == 3) {
+        filename1 = argv[1];
+        filename2 = argv[2];
+    }  else {
+        // direct input from the user
+        printf("File name 1:\n");
+        i = scanf("%s", filename1);
+        if(i != 1){
+            scanf_error();
+        }
+        printf("File name 2:\n");
+        i = scanf("%s", filename2);
+        if( i != 1){
+            scanf_error();
+        }
+    }
+
+    if(strcmp(filename1, filename2) == 0){
+        perror("File names cannot be named the same!");
+        exit(-1);
+    }
+
+    FILE* file1 = fopen(filename1, "r");
+    FILE* file2 = fopen(filename2, "w+");
+
+    char* character = calloc(2, sizeof (char));
+
+    unsigned int num;
+    while(!feof(file1)) { // better than using while true...
+        num = fread(character, 1, 1, file1);
+
+        if(feof(file1)){ // It is already the end of the file!
+            break;
+        }
+
+        if(!num){ // fread failed
+            perror("Fread error!");
+            exit(-1);
+        }
+
+        if(!isspace(character[0])) {
+            fwrite(character, 1, 1, file2);
+        }
+    }
+
+    fclose(file1);
+    fclose(file2);
+    if(argc != 3) {
+        free(filename1);
+        free(filename2);
+    }
+    return 0;
+}
+
+void scanf_error(){
+    perror("Scanf error!");
+    exit(-1);
+}
